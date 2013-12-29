9720b683c9a2a6f8b6ca5cf52a3c25a0d9e7e7e3
Tyler Breton <tcbreton@gmail.com>
2013-12-28 19:40:53 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rentpay::Application.config.secret_key_base = '19c899259428390e2bbb95c482e3b56903efc55f03f29dd0f8c89f6ab014c02d399ef8835d0547e18c26409e53a84c3e2a86ba0b2fc9d7075c8ff5304bbd5cf8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rentpay::Application.config.session_store :cookie_store, key: '_rentpay_session'
